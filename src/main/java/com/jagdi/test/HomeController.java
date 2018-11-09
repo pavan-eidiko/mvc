274cfab572af2255ec5729146cea1866026c8786
Jagadeesh Pagadala <Jagadeesh.Pagadala@jispl.com>
2018-11-09 11:37:18 +0530

    modified homeController

--- src/main/java/com/jagdi/test/HomeController.java
+++ src/main/java/com/jagdi/test/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
+		System.out.println("this is test");
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
